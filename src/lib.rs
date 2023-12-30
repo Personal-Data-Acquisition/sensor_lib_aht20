0ae167b3b742c9f139e47c180f7a1886afadc749
jakeg00dwin <jakegoodwin@gorge.works>
2023-12-29 21:24:16 -0800

    removed the template function and test modules that cargo added by default

--- src/lib.rs
+++ src/lib.rs
@@ -218,19 +218,3 @@ mod sensor_test {
     }
 }
 
-
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
